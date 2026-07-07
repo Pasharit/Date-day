ac60cc5101deb7574007854acf853002932d04f9
Pavlo Kikavsky <pokikavsky22@codingburgas.bg>
2026-07-07 17:01:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+setTimeout(() => {
+  document.getElementById("scene-loading").classList.remove("active");
+  document.getElementById("scene-forest").classList.add("active");
+}, 2400);
 
+const startBtn = document.getElementById("startJourney");
+
+startBtn.addEventListener("click", () => {
+  startBtn.innerText = "Скоро продолжим... 🌿";
+  startBtn.disabled = true;
+});
